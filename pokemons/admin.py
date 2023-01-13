c5c3c805c40496e3a7075a4f7ac12ebeb60ebf88
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2023-01-12 22:15:51 -0700

    feat: add list display for each admin model

--- pokemons/admin.py
+++ pokemons/admin.py
@@ -5,14 +5,17 @@ from .models import Pokemon, PokemonInstance, Person
 
 @admin.register(Pokemon)
 class PokemonAdmin(admin.ModelAdmin):
+  list_display = ('name', 'creator', 'created_at')
   pass
 
 
 @admin.register(PokemonInstance)
 class PokemonInstanceAdmin(admin.ModelAdmin):
+  list_display = ('pokemon', 'nickname', 'current_owner')
   pass
 
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
+  list_display = ('first_name', 'last_name', 'date_of_birth')
   pass
